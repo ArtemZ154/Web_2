1e394b67766573032acf594286b008f268dcaa90
ArtemZ154 <Artemz154@ya.ru>
2021-11-04 21:37:20 +0700

    HW Finish

--- Lesson31.10.21/server.js
+++ Lesson31.10.21/server.js
@@ -2,54 +2,64 @@ const express = require('express');
 const app = express();
 
 const inventory = {
-    '145623':{
-        'name':'Козырь',
-        'Money':'2000',
-        'discription':'Он ликвидирует своих врагов также уверенно, как поправляет галстук! Белый костюм, маска с красным ромбом, темные перчатки и галстук – лучше вам не видеть его за игрой в карты!'
-    },
-    '723532':{
-        'name':'Разведчица Galaxy',
-        'Money':'2000',
-        'discription':'Судьба галактики в ваших руках.'
-    },
-    '946362':{
-        'name':'Элитный агент',
-        'Money':'1500',
-        'discription':'Любой агент становится элитным, когда у него появляется средство для ночной видимости.'
-    },
-    '947394':{
-        'name':'Бёрди',
-        'Money':'800',
-        'discription':'Бьёт в цель с первой попытки.'
-    },
-    '561292':{
-        'name':'Вязаная связистка',
-        'Money':'2000',
-        'discription':'Связана из ласкутков одежды врагов.'
-    },
-    '123532':{
-        'name':'Позывной «Эльф»',
-        'Money':'1200',
-        'discription':'Я не могу объяснить ему отсутствие классических вытянутых ушек!'
-    },
-    '423276':{
-        'name':'Полковник Полуночник',
-        'Money':'1200',
-        'discription':'Любитель отдохнуть душой и телом.'
-    },
-    '847361':{
-        'name':'Весельчак',
-        'Money':'1500',
-        'discription':'Спокойных Вам кошмаров с таким персонажем!'
+    'users':{
+        '0':{
+            'name':'Козырь',
+            'Money':'2000',
+            'discription':'Он ликвидирует своих врагов также уверенно, как поправляет галстук! Белый костюм, маска с красным ромбом,    темные перчатки и галстук – лучше вам не видеть его за игрой в карты!',
+            'url': 'trump'
+        },
+        '1':{
+            'name':'Разведчица Galaxy',
+            'Money':'2000',
+            'discription':'Судьба галактики в ваших руках.',
+            'url': 'galaxy_scout'
+        },
+        '2':{
+            'name':'Элитный агент',
+            'Money':'1500',
+            'discription':'Любой агент становится элитным, когда у него появляется средство для ночной видимости.',
+            'url': 'elite_agent'
+        },
+        '3':{
+            'name':'Бёрди',
+            'Money':'800',
+            'discription':'Бьёт в цель с первой попытки.',
+            'url': 'birdie'
+        },
+        '4':{
+            'name':'Вязаная связистка',
+            'Money':'2000',
+            'discription':'Связана из ласкутков одежды врагов.',
+            'url': 'knitted_signalman'
+        },
+        '5':{
+            'name':'Позывной «Эльф»',
+            'Money':'1200',
+            'discription':'Я не могу объяснить ему отсутствие классических вытянутых ушек!',
+            'url': 'Called_Elf'
+        },
+        '6':{
+            'name':'Полковник Полуночник',
+            'Money':'1200',
+            'discription':'Любитель отдохнуть душой и телом.',
+            'url': 'colonel_midnight'
+        },
+        '7':{
+            'name':'Весельчак',
+            'Money':'1500',
+            'discription':'Спокойных Вам кошмаров с таким персонажем!',
+            'url': 'Humorist'
+        }
     }
-}
+};
 
 const profile = {
     'users':{
         0:{
             'nickname':'Genius',
             'level':'87',
-            'inventory':['145623', '723532', '946362', '561292', '947394'],
+            'inventory':['0', '1', '2', '4', '3'],
             'money':'555',
             'avatar':'..//Genius_a.jpg',
             'len_inv': '5'
@@ -57,7 +67,7 @@ const profile = {
         1:{
             'nickname':'Ghaechka',
             'level':'9999',
-            'inventory':['947394', '561292', '946362'],
+            'inventory':['3', '4', '2'],
             'money':'99999',
             'avatar':'..//Ghaechka_a.jpg',
             'len_inv': '3'
@@ -65,7 +75,7 @@ const profile = {
         2:{
             'nickname':'kloun_maks',
             'level':'73',
-            'inventory':['423276', '847361', '123532', '946362'],
+            'inventory':['6', '7', '5', '2'],
             'money':'350',
             'avatar':'..//kloun_maks_a.jpg',
             'len_inv': '4'
@@ -73,7 +83,7 @@ const profile = {
         3:{
             'nickname':'Admin',
             'level':'∞',
-            'inventory':['145623', '561292', '123532'],
+            'inventory':['0', '4', '5'],
             'money':'∞',
             'avatar':'..//Admin_a.jpg',
             'len_inv': '3'
@@ -81,13 +91,13 @@ const profile = {
     }
 };
 
-function qual_l_p() {
+function len_list(lst) {
     let quanity_profile = 0;
-    for (var key in profile.users) {
+    for (var key in lst.users) {
         quanity_profile++;
     };
     return quanity_profile;
-}
+};
 
 function prof_l() {
     let profile_last = {
@@ -96,12 +106,12 @@ function prof_l() {
         }
     };
     
-    for (let i = 0; i < qual_l_p(); i++) {
+    for (let i = 0; i < len_list(profile); i++) {
         let a = profile.users[i];
         profile_last.users[a.nickname] = a;
     };
     return profile_last;
-}
+};
 
 app.set('view engine', 'hbs');
 app.set('views', './templates');
@@ -120,7 +130,7 @@ app.get('/profile/:nickname/inventory', function(request, response) {
         prof:{
 
         }
-    }
+    };
     for (let i = 0; i < inventory_profile.length; i++) {
         a = inventory_profile[i];
         b = inventory[a]
@@ -132,12 +142,27 @@ app.get('/profile/:nickname/inventory', function(request, response) {
 app.get('/profiles', function(request, response) {
     data = {};
     all_profiles = [];
-    for (let i = 0; i < qual_l_p(); i++) {
+    for (let i = 0; i < len_list(profile); i++) {
         let a = profile.users[i].nickname;
         all_profiles.push(a);
     };
     data['all_profiles'] = all_profiles;
     response.render('profiles.hbs', data);
+});
+
+app.get('/items', function(request, response) {
+    data = {
+        items: {
+
+        }
+    };
+    for (let j = 0; j < len_list(inventory); j++) {
+        one_name = inventory.users[j].name;
+        url = inventory.users[j].url;
+        data.items[one_name] = url;
+    };
+    console.log(data)
+    response.render('items.hbs', data);
 });
 
 app.listen(port=3000, function() {
