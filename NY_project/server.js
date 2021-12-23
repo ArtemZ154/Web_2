97325f485e4a4f996acdc98ddc3ae0021181703f
ArtemZ154 <Artemz154@ya.ru>
2021-12-23 22:00:06 +0700

    hw 23.21.21 Finish

--- NY_project/server.js
+++ NY_project/server.js
@@ -1,64 +1,88 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { data } = require('jquery');
+var nunjucks = require('nunjucks') ;
 const app = express();
+var nunjucks = require('nunjucks') ;
 
-app.set('view engine', 'hbs');
-app.set('views', './templates');
+nunjucks.configure('templates', {
+    autoescape: true,
+    express: app
+});
 
+app.use(express.static('templates'));
 app.use(express.static('static'));
 
 const urlencodedParser = express.urlencoded({extended: false});
 
 let data_create_test = {
     
-}
+};
 
 app.get('/', function(request, response) {
-    let data = {
-        a:'as'
-    };
-    response.render('main.hbs', data);
+    let data = {};
+    response.render('main.njk', data);
 });
 
 app.get('/create_info', function(request, response){
-    let data = {
+    let data = {};
+    response.render('create_test.njk', data);
+});
 
-    };
-    response.render('create_test.hbs', data);
+app.get('/viewer_test/:numtest', function(request, response){
+    let num_test = request.params.numtest;
+    let data = {};
+    data = data_create_test[num_test];
+    console.log(data)
+    response.render('viewer_test.njk', data);
 });
 
 app.post('/info_test', urlencodedParser, function(req, res){
     que = {
 
-    }
+    };
     let count = 0;
     for (let key in data_create_test) {
         count++;
     };
-    console.log(req)
-    data = req.body;
-    data_create_test[count] = {}
+    let data = req.body;
+    data_create_test[count] = {};
     data_create_test[count]['name'] = data.name;
     data_create_test[count]['description'] = data['description_test'];
     all_aq = ((Object.keys(data)).length - 5) / 2;
+    data_create_test[count]['questions'] = {};
+    all_que_for_ans = []
+    all_que_for_con = {}
     for (let i = 0; i < data['quest_name'].length; i++) {
         quest = data['quest_name'];
-        a = quest[i]
-        que[i] = a
-        data_create_test[count]['questions'] = que
+        a = quest[i];
+        que[i] = a;
+        if (a in all_que_for_con) {
+            all_que_for_con[a] += 1
+            b = a + `(${all_que_for_con[a]})`;
+            data_create_test[count]['questions'][b]= '';
+            all_que_for_ans.push(b);
+        } else {
+            all_que_for_con[a] = 1;
+            data_create_test[count]['questions'][a]= '';
+            all_que_for_ans.push(a);
+        }
     };
-    all = {}
-    for (let i = 0; i < all_aq; i++) {
-        an = {}
-        for (let j = 0; j < all_aq; j++){
-            an[j] = data[`answer_question_input${i}`][j]
-        };
-        all[i] = an
+    for (let i = 0; i < all_que_for_ans.length; i++) {
+        a = all_que_for_ans[i];
+        data_create_test[count]['questions'][a] = data[`answer_question_input${i}`];
     };
-    data_create_test[count]['answers'] = all
-    console.log(data_create_test[0])
+    all_a_t = {
+
+    };
+    for (let i = 0; i < all_que_for_ans.length; i++) {
+        a = data[`answer_for_question${i}`];
+        all_a_t[i] = a;
+    };
+    data_create_test[count]['true_answers'] = all_a_t;
+    console.log(data_create_test)
 });
 
-app.listen(port=3000, function () {
+app.listen(port=8000, function () {
     console.log('Сервер запущен...');
 });
