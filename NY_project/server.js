3b924cb75cf8957e8c0ae26f71513c50107f9b6d
ArtemZ154 <Artemz154@ya.ru>
2021-12-24 19:10:59 +0700

    hw 24.12.21 Finish

--- NY_project/server.js
+++ NY_project/server.js
@@ -20,8 +20,9 @@ let data_create_test = {
     '0':{
         name: 'dvxz',
         description: 'zcxvcx',
-        questions: { adsfcx: [ 'adsfcxz', 'vedxzv' ], vvxczv: [ 'vcxzv', 'zxves' ] },
-        true_answers: { '0': 'adsfcx', '1': 'vcxzv' },
+        questions: { 'Знаешь физику?': [ 'да', 'нет', 'возможно' ], 'Знаешь алгебру?': [ 'да', 'нет', 'возможно' ], 'Знаешь геометрию?': [ 'да', 'нет', 'возможно' ] },
+        true_answers: { '0': 'нет', '1': 'да', '2': 'возможно' },
+        number_test: 0
       }
 };
 
@@ -86,8 +87,38 @@ app.post('/info_test', urlencodedParser, function(req, res){
         all_a_t[i] = a;
     };
     data_create_test[count]['true_answers'] = all_a_t;
+    data_create_test[count]['number_test'] = count
     res.redirect('/')
 });
+
+app.post("/result_test", urlencodedParser, function(req, res) {
+    let result_test_con = {}
+    let result_test_con_res = {}
+    let orachenie = []
+    let data = req.body;
+    let c = data_create_test[data.numbertest].questions;
+    c = Object.keys(c);
+    result_test = {}
+    for (let i = 0; i < (Object.keys(c)).length; i++) {
+        result_test_con[c[i]] = data[`answer-${c[i]}`]
+        orachenie.push(c[i])
+    };
+    for (let i = 0; i < (Object.keys(result_test_con)).length; i++) {
+        ans = result_test_con[orachenie[i]];
+        true_ans = data_create_test[data.numbertest]['true_answers'][i];
+        if (true_ans == ans) {
+            result_test_con_res[orachenie[i]] = 1
+        } else {
+            result_test_con_res[orachenie[i]] = 0
+        }
+    }
+    console.log(result_test_con_res)
+    res.redirect('/viewer_test_result')
+});
+
+app.get('/viewer_test_result', function(req, res){
+    
+})
 
 app.listen(port=8000, function () {
     console.log('Сервер запущен...');
