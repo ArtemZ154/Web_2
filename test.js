df6e3bb6a9b0cab767f67ca47bdeacfff0d6c6a2
ArtemZ154 <83006553+ArtemZ154@users.noreply.github.com>
2022-01-23 20:04:00 +0700

    Add files via upload

--- test.js
+++ test.js
@@ -1,21 +1,30 @@
-const sqlite = require("sqlite3");
+const sq = require("sqlite3");
+const express = require("express");
 
-let db = new sqlite.Database("genius_table.db", function(err) {
-    if (err){
-        console.log(err.message());
-    } else {
-        console.log("БД подключена!")
-    }
+async function getdata () {
+    let db = new sq.Database("genius_table.db");
+
+    let sqlq = "SELECT * FROM users"
+    let prom = new Promise(function(resolve, reject){
+        db.all(sqlq, [], function (err, rows){
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows)
+            };
+        });
+    });
+    let data = await prom;
+    db.close();
+    return data;
+}
+
+app = express();
+
+app.get("/", function (request, response){
+    getdata().then(function(data){
+        response.send(JSON.stringify(data));
+    });
 });
 
-
-db.serialize(function() {
-    let query = "SELECT * FROM users";
-    db.each(query, function (err, row) {
-        if (err){
-            console.log(err.message());
-        } else {
-            console.log(row)
-        }
-    })
-})
+app.listen(3000)
