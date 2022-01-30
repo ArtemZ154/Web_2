468e133bfa5e9993e9dc746e77e50d77b860d118
ArtemZ154 <83006553+ArtemZ154@users.noreply.github.com>
2022-01-30 19:59:40 +0700

    Add files via upload

--- test.js
+++ test.js
@@ -9,15 +9,19 @@ nunjucks.configure('templates', {
 });
 
 sqlq = 'SELECT * FROM Luchaya_tablica;';
-async function getdata (sqlq) {
+async function getdata (typeq, dataq) {
     let bd = new sq.Database('test_bd.db');
-
+    all_types = {
+        all:'SELECT * FROM Luchaya_tablica',
+        search:'SELECT * FROM Luchaya_tablica WHERE id=?'
+    };
+    let sqlq = all_types[typeq]
     let prom = new Promise(function(res, rej){
-        bd.all(sqlq, [], function(err, rows){
+        bd.all(sqlq, dataq, function(err, rows){
             if (err) {
                 rej(err);
             } else {
-                res(rows)
+                res(rows);
             };
         });
     });
@@ -27,7 +31,18 @@ async function getdata (sqlq) {
 };
 
 app.get('/', function(req, res){
-    getdata(sqlq).then(function(data){
+    getdata('all', []).then(function(data){
+        let templatedata = {
+            colums: Object.keys(data[0]),
+            rows: data
+        };
+        res.render('index.html', templatedata);
+    });
+});
+
+app.get('/search', function(req, res){
+    let data = Object.values(req.query);
+    getdata('search', data).then(function(data){
         let templatedata = {
             colums: Object.keys(data[0]),
             rows: data
